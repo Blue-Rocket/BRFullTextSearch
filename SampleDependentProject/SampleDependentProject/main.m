4066342747d0b66387c1d643f747a7ef8e9d7d7c
Matt Magoffin <git+matt@msqr.us>
2013-07-09 16:38:39 +1200

    Add sample project showing the dependent project integration approach.

---
+++ SampleDependentProject/SampleDependentProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleDependentProject
+//
+//  Created by Matt on 7/9/13.
+//  Copyright (c) 2013 Blue Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
