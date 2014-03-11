944e7e4b280196e26da52c4a0583b8def23a496f
Matt Magoffin <git+matt@msqr.us>
2014-03-11 16:24:21 +1300

    Fix iteration over Document Field instances; was converting *Fields into Fields by mistake.

--- BRFullTextSearch/CLuceneSearchResult.mm
+++ BRFullTextSearch/CLuceneSearchResult.mm
@@ -88,9 +88,9 @@ using namespace lucene::search;
 - (NSDictionary *)dictionaryRepresentation {
 	NSMutableDictionary *dict = [NSMutableDictionary new];
 	const Document &doc = hits->doc(index);
-	const Document::FieldsType fields = doc.getFields();
+	const Document::FieldsType *fields = doc.getFields();
 	Document::FieldsType::const_iterator itr;
-	for ( itr = fields.begin(); itr != fields.end(); itr++ ){
+	for ( itr = fields->begin(); itr != fields->end(); itr++ ){
 		Field *f = *itr;
 		NSString *fName = [NSString stringWithCLuceneString:f->name()];
 		NSString *fValue = [NSString stringWithCLuceneString:doc.get(f->name())];
