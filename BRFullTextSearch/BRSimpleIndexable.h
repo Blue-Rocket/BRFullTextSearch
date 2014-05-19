ddcd5a54c4bfa72b3d8085debeab126474f94645
Matt Magoffin <git+matt@msqr.us>
2014-05-20 09:40:02 +1200

    Add documentation.

--- BRFullTextSearch/BRSimpleIndexable.h
+++ BRFullTextSearch/BRSimpleIndexable.h
@@ -2,8 +2,6 @@
 //  BRSimpleIndexable.h
 //  BRFullTextSearch
 //
-//  A simple, dictionary-based implementation of BRIndexable.
-//
 //  Created by Matt on 7/5/13.
 //  Copyright (c) 2013 Blue Rocket. Distributable under the terms of the Apache License, Version 2.0.
 //
@@ -12,23 +10,55 @@
 
 extern const BRSearchObjectType kBRSimpleIndexableSearchObjectType;
 
+/**
+ * A simple, dictionary-based implementation of `BRIndexable`.
+ *
+ * This implementation of `BRIndexable` manages search fields interally as dictionary of field name
+ * keys and their associated values.
+ */
 @interface BRSimpleIndexable : NSObject <BRIndexable>
 
-// data aliases: setting these values modifies the data dictionary returned by indexFieldsDictionary
+/** Convenience property for accessing a `kBRSearchFieldNameTitle` field. */
 @property (nonatomic, retain) NSString *title;
+
+/** Convenience property for accessing a `kBRSearchFieldNameValue` field. */
 @property (nonatomic, retain) NSString *value;
+
+/** Convenience property for accessing a `kBRSearchFieldNameTimestamp` field. */
 @property (nonatomic, retain) NSDate *date;
 
-// propery alias for identifier
+/** Convenience property for accessing a `kBRSearchFieldNameIdentifier` field. */
 @property (nonatomic, readonly) NSString *uid;
 
-// init with kBRSimpleIndexableSearchObjectType and given identifier and data
-- (id)initWithIdentifier:(NSString *)identifier data:(NSDictionary *)data;
+/**
+ * Init with a specific unique identifier and field dictionary.
+ *
+ * The object type will be set to `kBRSimpleIndexableSearchObjectType`. A `kBRSearchFieldNameTimestamp` field
+ * will be populated automatically if one does not exist already in the provided field dictionary.
+ *
+ * @param identifier the unique identifer for this search document
+ * @param data the field values to associate with this search document
+ */
+- (instancetype)initWithIdentifier:(NSString *)identifier data:(NSDictionary *)data;
 
-// init with given attributes; a kBRSearchFieldNameTimestamp value is populated automatically
-- (id)initWithType:(BRSearchObjectType)type identifier:(NSString *)identifier data:(NSDictionary *)data;
+/**
+ * Init with a specific object type, unique identifier, and field dictionary.
+ *
+ * A `kBRSearchFieldNameTimestamp` field will be populated automatically if one does not exist already
+ * in the provided field dictionary.
+ *
+ * @param type the object type for this search document
+ * @param identifier the unique identifer for this search document
+ * @param data the field values to associate with this search document
+ */
+- (instancetype)initWithType:(BRSearchObjectType)type identifier:(NSString *)identifier data:(NSDictionary *)data;
 
-// directly modify a field value
+/**
+ * Directly modify the value for a field, or remove a field.
+ *
+ * @param object the field value to set, or `nil` to remove the field
+ * @param key the field name to modify
+ */
 - (void)setDataObject:(id)object forKey:(NSString *)key;
 
 @end
