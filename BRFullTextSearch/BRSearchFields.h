8015da95e2b969c5c8e2ffa1effab4be00c85ed3
Matt Magoffin <git+matt@msqr.us>
2014-05-20 06:28:08 +1200

    Add comments.

--- BRFullTextSearch/BRSearchFields.h
+++ BRFullTextSearch/BRSearchFields.h
@@ -10,16 +10,34 @@
 extern "C" {
 #endif
 
-extern NSString * const kBRSearchFieldNameIdentifier;
-extern NSString * const kBRSearchFieldNameObjectType;
-extern NSString * const kBRSearchFieldNameTitle;
-extern NSString * const kBRSearchFieldNameValue;
-extern NSString * const kBRSearchFieldNameTimestamp;
+extern NSString *const kBRSearchFieldNameIdentifier;
+extern NSString *const kBRSearchFieldNameObjectType;
+extern NSString *const kBRSearchFieldNameTitle;
+extern NSString *const kBRSearchFieldNameValue;
+extern NSString *const kBRSearchFieldNameTimestamp;
 
+/** Type definition for a "type of" search object flag. */
 typedef char BRSearchObjectType;
 
-extern NSString * StringForBRSearchObjectType(BRSearchObjectType type);
-extern BRSearchObjectType BRSearchObjectTypeForString(NSString * string);
+/**
+ * Create a string from a `BRSearchObjectType`.
+ *
+ * @param type the search object type
+ * @return the search object type as a string
+ */
+extern NSString *StringForBRSearchObjectType(BRSearchObjectType type);
+
+/**
+ * Create a `BRSearchObjectType` from a string.
+ *
+ * If the given string does not have at least one character in it, the null
+ * character will be returned (`\0`). Otherwise the string will be interpreted
+ * as ASCII characters and the first character returned.
+ *
+ * @param string the string
+ * @return the search object type
+ */
+extern BRSearchObjectType BRSearchObjectTypeForString(NSString *string);
 
 #ifdef __cplusplus
 }
