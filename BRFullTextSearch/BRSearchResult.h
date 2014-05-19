96bdbbf42eee411e8689dadbbd760d9ee290c236
Matt Magoffin <git+matt@msqr.us>
2014-05-20 06:41:24 +1200

    Add comments.

--- BRFullTextSearch/BRSearchResult.h
+++ BRFullTextSearch/BRSearchResult.h
@@ -10,21 +10,48 @@
 
 #import "BRSearchFields.h"
 
+/**
+ * API for a search result match.
+ *
+ * When performing searches, individual documents that match the query are returned as
+ * objects conforming to this protocol. This API closely mirrors the `BRIndexable` API.
+ *
+ * @see BRIndexable
+ */
 @protocol BRSearchResult <NSObject>
 
+/** Get the search document's object type for the match. */
 @property (nonatomic, readonly) BRSearchObjectType objectType;
 
-// some unique identifier (unique to objectType) for the result
+/** Get the search document's unique identifier for the match. */
 @property (nonatomic, readonly) NSString *identifier;
 
-// get the value of a field
+/**
+ * Get the value of a field.
+ *
+ * @param fieldName the name of the field to get the value for
+ * @return the value of the field, or `nil` if no value is available
+ */
 - (id)valueForField:(NSString *)fieldName;
 
-// get a date components, in local time, for a given timestamp field
+/**
+ * Get the date components, in local time, for a timestamp field.
+ *
+ * @param fieldName the name of a field whose value is a timestamp
+ * @return the timestamp converted to a `NSDateComponents` in the runtime's current time zone
+ */
 - (NSDateComponents *)localDayForTimestampField:(NSString *)fieldName;
 
-// get the values for all fields (including the identifier and object type) as a dictionary;
-// if a field contains more than one value the values will be stored as an array in the returned dictionary
+/**
+ * Get all available field values as a dictionary.
+ *
+ * The keys in the returned dictionary represent the available field names in the search
+ * document. The dictionary will include the search document's unique identifier and object type
+ * (as `NSString` values). If a field contains more than one value the values will be stored in
+ * a `NSArray` in the returned dictionary.
+ *
+ * @return a dictionary of all available field values
+ */
 - (NSDictionary *)dictionaryRepresentation;
 
 @end
