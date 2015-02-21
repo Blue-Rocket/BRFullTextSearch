eb5342b94d5535fc3bef3d3ff0a10ee1688e546b
Matt Magoffin <git+matt@msqr.us>
2015-02-21 14:23:25 +1300

    Fix encoding of arbitrary text values into Lucene strings. Previous method produced undefined results sometimes, due to using cStringUsingEncoding: not supporting UTF32 (this is documented in the Apple documentation).

--- BRFullTextSearch/NSString+CLuceneAdditions.h
+++ BRFullTextSearch/NSString+CLuceneAdditions.h
@@ -8,12 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
-#import "CLucene.h"
+#import "CLucene/_ApiHeader.h"
 
 @interface NSString (CLuceneAdditions)
 
-// these helpers aid in dealing with CLucene's wchar_t use for strings
+/**
+ Get a Lucene character string. Note the returned data is only valid for as long as the receiver
+ is valid, and only "simple" strings (like field names) should use this. For full character support
+ use the @c asCLuceneStringData method.
+ 
+ @return A pointer to character data encoded as a null-terminated Lucene string.
+ */
 - (const TCHAR *)asCLuceneString;
+
+/**
+ Get a null-terminated Lucene string data object. This is suitable for all Lucene strings,
+ and the caller can call @c asCLuceneString on the returned NSData to get the TCHAR data.
+ 
+ @return A new NSData with bytes copied from the receiver.
+ */
+- (NSData *)asCLuceneStringData;
+
 + (NSString *)stringWithCLuceneString:(const TCHAR *)charText;
 
 @end
